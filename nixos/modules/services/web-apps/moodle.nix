450a099c0f31f2a5cac62fc1e5acbe1ff0a48380
Finn Behrens <me@kloenk.dev>
2022-07-11 08:41:18 +0200

    nixos/moodle: apply recomended php extension

--- nixos/modules/services/web-apps/moodle.nix
+++ nixos/modules/services/web-apps/moodle.nix
@@ -57,7 +57,7 @@ let
   pgsqlLocal = cfg.database.createLocally && cfg.database.type == "pgsql";
 
   phpExt = pkgs.php81.withExtensions
-        ({ enabled, all }: with all; [ iconv mbstring curl openssl tokenizer xmlrpc soap ctype zip gd simplexml dom  intl json sqlite3 pgsql pdo_sqlite pdo_pgsql pdo_odbc pdo_mysql pdo mysqli session zlib xmlreader fileinfo filter opcache ]);
+        ({ enabled, all }: with all; [ iconv mbstring curl openssl tokenizer soap ctype zip gd simplexml dom  intl sqlite3 pgsql pdo_sqlite pdo_pgsql pdo_odbc pdo_mysql pdo mysqli session zlib xmlreader fileinfo filter opcache exif sodium ]);
 in
 {
   # interface
