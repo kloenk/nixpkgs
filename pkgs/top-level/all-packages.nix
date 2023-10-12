3babe702bff47f07cd67ee7b9589ccad4c48c08a
Scott Worley <scottworley@scottworley.com>
2023-10-11 17:29:11 -0700

    pico-sdk: Make full SDK available. The old pico-sdk is now pico-sdk-minimal

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -24030,6 +24030,7 @@ with pkgs;
     physfs;
 
   pico-sdk = callPackage ../development/libraries/pico-sdk { };
+  pico-sdk-minimal = pico-sdk.override { minimal = true; };
 
   pinocchio = callPackage ../development/libraries/pinocchio { };
 
